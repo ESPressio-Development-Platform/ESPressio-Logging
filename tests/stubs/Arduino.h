d1e159db5055c0c30cb41ce327259c2fc7cb4b30
Simon J Stuart <lakraven@gmail.com>
2026-09-01 18:11:40 +0200

    Add host Arduino compatibility stub for Logging tests

---
+++ tests/stubs/Arduino.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+class String {
+private:
+    std::string _value;
+
+public:
+    String(const char* value = "") : _value(value) {}
+
+    String& operator+=(const char* value) {
+        _value += value;
+        return *this;
+    }
+
+    const char* c_str() const {
+        return _value.c_str();
+    }
+};
+
+inline uint32_t& ArduinoMicrosValue() {
+    static uint32_t value = 0;
+    return value;
+}
+
+inline uint32_t micros() {
+    return ArduinoMicrosValue();
+}
